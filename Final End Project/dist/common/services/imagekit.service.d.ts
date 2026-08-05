91a733787405ea3314f7c2a2f04da634c0bb5df4
Z-Haneen <haneenwael6@gmail.com>
2026-08-05 04:17:13 +0300

    refactor: remove redundant Final End Project directory

--- Final End Project/dist/common/services/imagekit.service.d.ts
+++
@@ -1,8 +0,0 @@
-import 'multer';
-import { ConfigService } from '@nestjs/config';
-export declare class ImageKitService {
-    private configService;
-    private imagekit;
-    constructor(configService: ConfigService);
-    uploadImage(file: Express.Multer.File, folder?: string): Promise<string>;
-}
